757f947cbbd69ff831c61f4e1a2b4a1a8c86ad33
isri21 <israelalemu21@gmail.com>
2025-07-06 21:07:25 +0300

    Task 3 #1

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,3 +1,4 @@
+-- Write SQL CREATE INDEX commands to create appropriate indexes for those columns and save them on database_index.sql
 CREATE INDEX idx_users_user_id ON Users(user_id);
 CREATE INDEX idx_bookings_user_id ON Bookings(user_id);
 CREATE INDEX idx_bookings_property_id ON Bookings(property_id);
@@ -6,6 +7,7 @@ CREATE INDEX idx_properties_property_id ON Properties(property_id);
 CREATE INDEX idx_properties_location ON Properties(location);
 CREATE INDEX idx_properties_host_id ON Properties(host_id);
 
+-- Measure the query performance before and after adding indexes using EXPLAIN or ANALYZE.
 EXPLAIN ANALYZE
 SELECT u.name, p.title, b.start_date, b.total_price
 FROM Bookings b
